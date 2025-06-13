f38798423ebc6a1d1cf0f89deb7b1c7727a0c02a
i532006 <gustavo.conceicao@sap.com>
2025-06-13 20:50:48 -0300

    Added model and repository

--- expense-service/src/main/java/com/example/expense/model/Expense.java
+++ expense-service/src/main/java/com/example/expense/model/Expense.java
@@ -1,4 +1,51 @@
 package com.example.expense.model;
 
+import java.math.BigDecimal;
+
 public class Expense {
+
+    private long id;
+
+    private String Description;
+
+    private String category;
+
+
+    private BigDecimal value;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+
+
 }
---
+++ expense-service/src/main/java/com/example/expense/repository/ExpenseRepository.java
@@ -0,0 +1,15 @@
+package com.example.expense.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.expense.model.Expense;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    
+
+
+    
+}
