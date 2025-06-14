e9bba66338e3ceb83986998e49f525d52b22e6b8
i532006 <gustavo.conceicao@sap.com>
2025-06-14 17:59:17 -0300

    added get Expense by ID

--- expense-service/src/main/java/com/example/expense/controller/ExpenseController.java
+++ expense-service/src/main/java/com/example/expense/controller/ExpenseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,9 +28,16 @@ public class ExpenseController {
     public List<ExpenseResponse> getAllExpenses(){
         return expenseService.getAllExpenses();
     }
+
+    @GetMapping("/{id}")
+    public ExpenseResponse getExpenseByID(@PathVariable("id") long id){
+        return expenseService.getExpenseById(id);
+    }
     
     @PostMapping
     public Expense creatExpense(@RequestBody CreateExpenseRequest expense){
         return expenseService.saveExpense(expense);
     }
+
+
 }
--- expense-service/src/main/java/com/example/expense/service/ExpenseService.java
+++ expense-service/src/main/java/com/example/expense/service/ExpenseService.java
@@ -1,6 +1,7 @@
 package com.example.expense.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -39,6 +40,21 @@ public class ExpenseService {
         response.setDate(expense.getDate());
         response.setDescription(expense.getDescription());
         response.setId(expense.getId());
+        response.setValueSpent(expense.getValueSpent());
+
+        return response;
+    }
+
+    public ExpenseResponse getExpenseById(Long id){
+        ExpenseResponse response = new ExpenseResponse();
+
+        Expense expense= expenseRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task with ID " + id + " not found"));
+
+        response.setId(expense.getId());
+        response.setCategory(expense.getCategory());
+        response.setDate(expense.getDate());
+        response.setDescription(expense.getDescription());
         response.setValueSpent(expense.getValueSpent());
 
         return response;
