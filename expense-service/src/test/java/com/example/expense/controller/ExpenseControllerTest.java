234366aad972fcb086449756ebf676e489a0158b
guustavomc <gustavomccb@gmail.com>
2025-06-30 21:48:57 -0300

    Added Unit test for Expense Controller

--- expense-service/src/test/java/com/example/expense/controller/ExpenseControllerTest.java
+++ expense-service/src/test/java/com/example/expense/controller/ExpenseControllerTest.java
@@ -1,4 +1,79 @@
 package com.example.expense.controller;
 
+import com.example.expense.dto.ExpenseResponse;
+import com.example.expense.repository.ExpenseRepository;
+import com.example.expense.service.ExpenseService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ExpenseControllerTest {
+
+    @Mock
+    private ExpenseService expenseService;
+
+    @InjectMocks
+    private ExpenseController expenseController;
+
+    @Test
+    void getAllExpenses_ReturnListOfResponseEntity(){
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+        List<ExpenseResponse> expenseResponseList = new ArrayList<>();
+        expenseResponseList.add(expenseResponse);
+        when(expenseService.findAllExpenses()).thenReturn(expenseResponseList);
+
+        ResponseEntity<List<ExpenseResponse>> response = expenseController.getAllExpenses();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().size());
+        assertEquals(expenseResponseList, response.getBody());
+
+    }
+
+    @Test
+    void getAllExpenses_ReturnNotFound(){
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+        List<ExpenseResponse> expenseResponseList = new ArrayList<>();
+        expenseResponseList.add(expenseResponse);
+        when(expenseService.findAllExpenses()).thenThrow(new RuntimeException());
+
+        ResponseEntity<List<ExpenseResponse>> response = expenseController.getAllExpenses();
+
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+    }
+
+    @Test
+    void getExpenseByID_ReturnExpenseResponse(){
+        long id = 1L;
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+
+        when(expenseService.findExpenseById(id)).thenReturn(expenseResponse);
+
+        ResponseEntity<ExpenseResponse> response = expenseController.getExpenseByID(id);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(expenseResponse, response.getBody());
+    }
+
+    @Test
+    void getExpenseByID_ReturnNotFound(){
+        long id = 1L;
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+
+        when(expenseService.findExpenseById(id)).thenThrow(new RuntimeException());
+
+        ResponseEntity<ExpenseResponse> response = expenseController.getExpenseByID(id);
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+    }
 }
+
