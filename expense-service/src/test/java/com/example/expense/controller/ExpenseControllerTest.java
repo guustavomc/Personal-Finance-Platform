11db590ba2281bca9be58d374e2f219eccd3bfee
guustavomc <gustavomccb@gmail.com>
2025-06-30 22:38:12 -0300

    Added Unit test for Expense Controller

--- expense-service/src/test/java/com/example/expense/controller/ExpenseControllerTest.java
+++ expense-service/src/test/java/com/example/expense/controller/ExpenseControllerTest.java
@@ -75,5 +75,63 @@ public class ExpenseControllerTest {
         ResponseEntity<ExpenseResponse> response = expenseController.getExpenseByID(id);
         assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
     }
+
+    @Test
+    void getExpensesWithCategory_ReturnExpenseResponseList_WithCategory(){
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+        List<ExpenseResponse> expenseResponseList = new ArrayList<>();
+        expenseResponseList.add(expenseResponse);
+        String category = "Health";
+
+        when(expenseService.findExpensesByCategory(category)).thenReturn(expenseResponseList);
+
+        ResponseEntity<List<ExpenseResponse>> response = expenseController.getExpensesWithCategory(category);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().size());
+
+    }
+
+    @Test
+    void getExpensesWithCategory_ReturnNotFound(){
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+        List<ExpenseResponse> expenseResponseList = new ArrayList<>();
+        expenseResponseList.add(expenseResponse);
+        String category = "Health";
+
+        when(expenseService.findExpensesByCategory(category)).thenThrow(new RuntimeException());
+
+        ResponseEntity<List<ExpenseResponse>> response = expenseController.getExpensesWithCategory(category);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+    }
+
+    @Test
+    void getMonthlyExpenses_ReturnExpenseResponseList_WithMonthlyExpenses(){
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+        List<ExpenseResponse> expenseResponseList = new ArrayList<>();
+        expenseResponseList.add(expenseResponse);
+        int year = 2023;
+        int month = 6;
+
+        when(expenseService.findExpensesByMonth(year, month)).thenReturn(expenseResponseList);
+
+        ResponseEntity<List<ExpenseResponse>> response = expenseController.getMonthlyExpenses(year, month);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().size());
+    }
+
+    @Test
+    void getMonthlyExpenses_ReturnNotFound(){
+        ExpenseResponse expenseResponse = new ExpenseResponse();
+        List<ExpenseResponse> expenseResponseList = new ArrayList<>();
+        expenseResponseList.add(expenseResponse);
+        int year = 2023;
+        int month = 6;
+
+        when(expenseService.findExpensesByMonth(year, month)).thenThrow(new RuntimeException());
+
+        ResponseEntity<List<ExpenseResponse>> response = expenseController.getMonthlyExpenses(year, month);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
 }
 
