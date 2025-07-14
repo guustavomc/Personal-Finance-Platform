9c7a7fa60c90ba76da417d659d20a9bff2b636f2
guustavomc <gustavomccb@gmail.com>
2025-07-14 08:44:07 -0300

    GlobalExceptionHandler created, centralizing exception handling and returning appropriate error details

---
+++ expense-service/src/main/java/com/example/expense/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.expense.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    public ResponseEntity<Object> handleExpenseNotFoundException(ExpenseNotFoundException ex){
+        Map<String,Object> response = new HashMap<>();
+        response.put("timestamp", LocalDate.now());
+        response.put("status", HttpStatus.NOT_FOUND);
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+
+    }
+}
