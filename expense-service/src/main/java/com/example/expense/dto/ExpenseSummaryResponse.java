f4710ea0405629093cac6aef3fde263824d9c55b
guustavomc <gustavomccb@gmail.com>
2025-07-21 22:18:31 -0300

    Created ExpenseSummaryResponse dto

---
+++ expense-service/src/main/java/com/example/expense/dto/ExpenseSummaryResponse.java
@@ -0,0 +1,49 @@
+package com.example.expense.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ExpenseSummaryResponse {
+
+    private BigDecimal totalExpenses;
+    private BigDecimal averageDailyExpense;
+    private String highestSpentCategory;
+
+    List<ExpenseResponse> detailedExpenses;
+
+    public ExpenseSummaryResponse(){
+
+    }
+
+    public BigDecimal getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public BigDecimal getAverageDailyExpense() {
+        return averageDailyExpense;
+    }
+
+    public String getHighestSpentCategory() {
+        return highestSpentCategory;
+    }
+
+    public List<ExpenseResponse> getDetailedExpenses() {
+        return detailedExpenses;
+    }
+
+    public void setTotalExpenses(BigDecimal totalExpenses) {
+        this.totalExpenses = totalExpenses;
+    }
+
+    public void setAverageDailyExpense(BigDecimal averageDailyExpense) {
+        this.averageDailyExpense = averageDailyExpense;
+    }
+
+    public void setHighestSpentCategory(String highestSpentCategory) {
+        this.highestSpentCategory = highestSpentCategory;
+    }
+
+    public void setDetailedExpenses(List<ExpenseResponse> detailedExpenses) {
+        this.detailedExpenses = detailedExpenses;
+    }
+}
