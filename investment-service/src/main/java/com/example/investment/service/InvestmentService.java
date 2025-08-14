c23fbddf88d58f3f1fa5eabcbf32fb6b9a919526
guustavomc <gustavomccb@gmail.com>
2025-08-13 21:09:37 -0300

    create service methods

--- investment-service/src/main/java/com/example/investment/service/InvestmentService.java
+++ investment-service/src/main/java/com/example/investment/service/InvestmentService.java
@@ -1,4 +1,49 @@
 package com.example.investment.service;
 
+import com.example.investment.dto.InvestmentResponse;
+import com.example.investment.model.Investment;
+import com.example.investment.repository.InvestmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class InvestmentService {
+
+    private InvestmentRepository investmentRepository;
+
+    public InvestmentService (InvestmentRepository investmentRepository){
+        this.investmentRepository=investmentRepository;
+    }
+
+    public List<InvestmentResponse> findAllInvestmentsMade(){
+        return  investmentRepository.findAll().stream().map(investment -> mapInvestmentToInvestmentResponse(investment)).toList();
+    }
+
+    public InvestmentResponse findInvestmentTransactionWithID(Long id){
+        Investment investment = findVerifiedInvestmentTransactionWithID(id);
+        return mapInvestmentToInvestmentResponse(investment);
+    }
+
+    public Investment findVerifiedInvestmentTransactionWithID(Long id){
+        return investmentRepository.findById(id)
+                .orElseThrow(() ->new RuntimeException("Investment not found"));
+
+    }
+
+    public InvestmentResponse mapInvestmentToInvestmentResponse(Investment investment){
+        InvestmentResponse response = new InvestmentResponse();
+        response.setId(investment.getId());
+        response.setAssetSymbol(investment.getAssetSymbol());
+        response.setAmountInvested(investment.getAmountInvested());
+        response.setQuantity(investment.getQuantity());
+        response.setInvestmentDate(investment.getInvestmentDate());
+        response.setCurrency(investment.getCurrency());
+        response.setAlternateAmount(investment.getAlternateAmount());
+        response.setAlternateCurrency(investment.getAlternateCurrency());
+
+        return response;
+    }
+
+
 }
