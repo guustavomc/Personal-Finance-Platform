a311b920324260383a8c24e68bdcb3e5e21d7d06
guustavomc <gustavomccb@gmail.com>
2025-08-20 22:34:32 -0300

    updated Create expense dto to contain installment info and total purchase information

--- expense-service/src/main/java/com/example/expense/dto/CreateExpenseRequest.java
+++ expense-service/src/main/java/com/example/expense/dto/CreateExpenseRequest.java
@@ -1,5 +1,6 @@
 package com.example.expense.dto;
 
+import com.example.expense.model.PaymentMethod;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,10 +17,14 @@ public class CreateExpenseRequest {
 
     @NotNull(message = "Amount is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Amount must be greater than zero")
-    private BigDecimal valueSpent;
+    private BigDecimal totalPurchaseValue;
 
     @NotNull(message = "Date is required")
     private LocalDate date;
+
+    private PaymentMethod paymentMethod;
+
+    private int numberOfInstallments = 1;
 
     public String getDescription() {
         return description;
@@ -28,14 +33,23 @@ public class CreateExpenseRequest {
     public String getCategory() {
         return category;
     }
-    
-    public BigDecimal getValueSpent() {
-        return valueSpent;
+
+    public BigDecimal getTotalPurchaseValue() {
+        return totalPurchaseValue;
     }
 
     public LocalDate getDate() {
         return date;
     }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public int getNumberOfInstallments() {
+        return numberOfInstallments;
+    }
+
 
     public void setDescription(String description) {
         this.description = description;
@@ -45,11 +59,21 @@ public class CreateExpenseRequest {
         this.category = category;
     }
 
-    public void setValueSpent(BigDecimal valueSpent) {
-        this.valueSpent = valueSpent;
+    public void setTotalPurchaseValue(BigDecimal totalPurchaseValue) {
+        this.totalPurchaseValue = totalPurchaseValue;
     }
 
     public void setDate(LocalDate date) {
         this.date = date;
     }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public void setNumberOfInstallments(int numberOfInstallments) {
+        this.numberOfInstallments = numberOfInstallments;
+    }
+
+
 }
