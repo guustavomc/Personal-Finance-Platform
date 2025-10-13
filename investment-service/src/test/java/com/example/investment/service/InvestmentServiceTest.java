54b4b89d3926f4d7858c297bf41067596bbc9463
guustavomc <gustavomccb@gmail.com>
2025-10-13 08:33:14 -0300

    implemented Investment Service Unit Tests

--- investment-service/src/test/java/com/example/investment/service/InvestmentServiceTest.java
+++ investment-service/src/test/java/com/example/investment/service/InvestmentServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.investment.service;
 
+import com.example.investment.dto.CreateInvestmentRequest;
 import com.example.investment.dto.InvestmentResponse;
 import com.example.investment.model.Investment;
 import com.example.investment.repository.InvestmentRepository;
@@ -18,8 +19,9 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.example.investment.model.InvestmentType.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class InvestmentServiceTest {
@@ -83,5 +85,173 @@ public class InvestmentServiceTest {
         assertEquals(investmentResponses.getInvestmentDate(),
                 LocalDate.of(2025, 10, 8));
         assertEquals(investmentResponses.getCurrency(),"BRL");
+    }
+
+    @Test
+    void saveInvestment_ReturnInvestmentResponseCreated(){
+        Investment investment = new Investment();
+        investment.setId(1L);
+        investment.setInvestmentType(CRYPTO);
+        investment.setAssetSymbol("BTC");
+        investment.setAmountInvested(BigDecimal.valueOf(1000));
+        investment.setQuantity(BigDecimal.valueOf(0.1));
+        investment.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        investment.setCurrency("BRL");
+
+        CreateInvestmentRequest createInvestmentRequest = new CreateInvestmentRequest();
+        createInvestmentRequest.setInvestmentType(CRYPTO);
+        createInvestmentRequest.setAssetSymbol("BTC");
+        createInvestmentRequest.setAmountInvested(BigDecimal.valueOf(1000));
+        createInvestmentRequest.setQuantity(BigDecimal.valueOf(0.1));
+        createInvestmentRequest.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        createInvestmentRequest.setCurrency("BRL");
+
+        when(investmentRepository.save(any(Investment.class))).thenReturn(investment);
+        InvestmentResponse investmentResponse = investmentService.saveInvestment(createInvestmentRequest);
+
+        assertNotNull(investmentResponse);
+        assertEquals(investmentResponse.getInvestmentType(),CRYPTO);
+        assertEquals(investmentResponse.getAssetSymbol(),"BTC");
+        assertEquals(investmentResponse.getAmountInvested(),BigDecimal.valueOf(1000));
+        assertEquals(investmentResponse.getQuantity(),BigDecimal.valueOf(0.1));
+        assertEquals(investmentResponse.getInvestmentDate(),
+                LocalDate.of(2025, 10, 8));
+        assertEquals(investmentResponse.getCurrency(),"BRL");
+    }
+
+    @Test
+    void saveInvestment_ThrowRunTimeException_WhenFailedToSaveInvestment(){
+        Investment investment = new Investment();
+        investment.setId(1L);
+        investment.setInvestmentType(CRYPTO);
+        investment.setAssetSymbol("BTC");
+        investment.setAmountInvested(BigDecimal.valueOf(1000));
+        investment.setQuantity(BigDecimal.valueOf(0.1));
+        investment.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        investment.setCurrency("BRL");
+
+        CreateInvestmentRequest createInvestmentRequest = new CreateInvestmentRequest();
+        createInvestmentRequest.setInvestmentType(CRYPTO);
+        createInvestmentRequest.setAssetSymbol("BTC");
+        createInvestmentRequest.setAmountInvested(BigDecimal.valueOf(1000));
+        createInvestmentRequest.setQuantity(BigDecimal.valueOf(0.1));
+        createInvestmentRequest.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        createInvestmentRequest.setCurrency("BRL");
+
+        when(investmentRepository.save(any(Investment.class))).thenThrow(new RuntimeException());
+
+        assertThrows(RuntimeException.class, ()-> investmentService.saveInvestment(createInvestmentRequest));
+    }
+
+    @Test
+    void removeInvestment_DeleteInvestment_WithID(){
+        when(investmentRepository.existsById(1L)).thenReturn(true);
+        doNothing().when(investmentRepository).deleteById(1L);
+
+        investmentService.removeInvestment(1L);
+        verify(investmentRepository, times(1)).existsById(1L);
+        verify(investmentRepository, times(1)).deleteById(1L);
+    }
+    @Test
+    void removeInvestment_ThrowRunTimeException_WhenInvestmentNotFound(){
+        long id = 1L;
+
+        when(investmentRepository.existsById(id)).thenReturn(false);
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                ()->investmentService.removeInvestment(id));
+        assertEquals("Investment with ID " + id + " not found", exception.getMessage());
+    }
+
+    @Test
+    void removeInvestment_ThrowRunTimeException_WhenFailedToDeleteInvestment(){
+        long id = 1L;
+
+        when(investmentRepository.existsById(1L)).thenReturn(true);
+        doThrow(new RuntimeException("Database error")).when(investmentRepository).deleteById(id);
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                ()->investmentService.removeInvestment(id));
+        assertEquals("Failed to delete Investment with ID " + id, exception.getMessage());
+    }
+
+    @Test
+    void editInvestmentWithID_EditExpense_ReturnInvestmentResponse(){
+        Investment investment = new Investment();
+        investment.setId(1L);
+        investment.setInvestmentType(CRYPTO);
+        investment.setAssetSymbol("BTC");
+        investment.setAmountInvested(BigDecimal.valueOf(1000));
+        investment.setQuantity(BigDecimal.valueOf(0.1));
+        investment.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        investment.setCurrency("BRL");
+
+        CreateInvestmentRequest createInvestmentRequest = new CreateInvestmentRequest();
+        createInvestmentRequest.setInvestmentType(CRYPTO);
+        createInvestmentRequest.setAssetSymbol("BTC");
+        createInvestmentRequest.setAmountInvested(BigDecimal.valueOf(1000));
+        createInvestmentRequest.setQuantity(BigDecimal.valueOf(0.2));
+        createInvestmentRequest.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        createInvestmentRequest.setCurrency("BRL");
+
+        Investment investmentUpdated = new Investment();
+        investmentUpdated.setId(1L);
+        investmentUpdated.setInvestmentType(CRYPTO);
+        investmentUpdated.setAssetSymbol("BTC");
+        investmentUpdated.setAmountInvested(BigDecimal.valueOf(1000));
+        investmentUpdated.setQuantity(BigDecimal.valueOf(0.2));
+        investmentUpdated.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        investmentUpdated.setCurrency("BRL");
+
+        when(investmentRepository.findById(1L)).thenReturn(Optional.of(investment));
+        when(investmentRepository.save(any(Investment.class))).thenReturn(investmentUpdated);
+
+        InvestmentResponse investmentResponse = investmentService.
+                editInvestmentById(1L, createInvestmentRequest);
+
+        assertEquals(investmentResponse.getInvestmentType(),CRYPTO);
+        assertEquals(investmentResponse.getAssetSymbol(),"BTC");
+        assertEquals(investmentResponse.getAmountInvested(),BigDecimal.valueOf(1000));
+        assertEquals(investmentResponse.getQuantity(),BigDecimal.valueOf(0.2));
+        assertEquals(investmentResponse.getInvestmentDate(),
+                LocalDate.of(2025, 10, 8));
+        assertEquals(investmentResponse.getCurrency(),"BRL");
+    }
+
+    @Test
+    void editInvestmentById_ThrowRunTimeException_WhenFailedToEditInvestment(){
+        Investment investment = new Investment();
+        investment.setId(1L);
+        investment.setInvestmentType(CRYPTO);
+        investment.setAssetSymbol("BTC");
+        investment.setAmountInvested(BigDecimal.valueOf(1000));
+        investment.setQuantity(BigDecimal.valueOf(0.1));
+        investment.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        investment.setCurrency("BRL");
+
+        CreateInvestmentRequest createInvestmentRequest = new CreateInvestmentRequest();
+        createInvestmentRequest.setInvestmentType(CRYPTO);
+        createInvestmentRequest.setAssetSymbol("BTC");
+        createInvestmentRequest.setAmountInvested(BigDecimal.valueOf(1000));
+        createInvestmentRequest.setQuantity(BigDecimal.valueOf(0.2));
+        createInvestmentRequest.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        createInvestmentRequest.setCurrency("BRL");
+
+        Investment investmentUpdated = new Investment();
+        investment.setId(1L);
+        investment.setInvestmentType(CRYPTO);
+        investment.setAssetSymbol("BTC");
+        investment.setAmountInvested(BigDecimal.valueOf(1000));
+        investment.setQuantity(BigDecimal.valueOf(0.2));
+        investment.setInvestmentDate(LocalDate.of(2025, 10, 8));
+        investment.setCurrency("BRL");
+
+        when(investmentRepository.findById(1L)).thenReturn(Optional.of(investment));
+        doThrow(new RuntimeException("Database error"))
+                .when(investmentRepository).save(investmentUpdated);
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                ()->investmentService.editInvestmentById(1L,createInvestmentRequest));
+        assertEquals("Failed to Edit Investment", exception.getMessage());
     }
 }
