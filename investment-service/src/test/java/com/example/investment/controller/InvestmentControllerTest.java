da89ce69451121410d7b5384f38d53356866cc64
guustavomc <gustavomccb@gmail.com>
2025-10-17 08:40:54 -0300

    implemented Investment Controller unit test

--- investment-service/src/test/java/com/example/investment/controller/InvestmentControllerTest.java
+++ investment-service/src/test/java/com/example/investment/controller/InvestmentControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.investment.controller;
 
+import com.example.investment.dto.InvestmentResponse;
 import com.example.investment.service.InvestmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class InvestmentControllerTest {
@@ -23,7 +32,19 @@ public class InvestmentControllerTest {
     }
 
     @Test
-    void getAllInvestmentsMade_ReturnResponseEntity_WithInvestmentResponseList(){}
+    void getAllInvestmentsMade_ReturnResponseEntity_WithInvestmentResponseList(){
+        InvestmentResponse investmentResponse = new InvestmentResponse();
+        List<InvestmentResponse> investmentResponseList = new ArrayList<>();
+
+        investmentResponseList.add(investmentResponse);
+
+        when(investmentService.findAllInvestmentsMade()).thenReturn(investmentResponseList);
+
+        ResponseEntity<List<InvestmentResponse>> response = investmentController.getAllInvestmentsMade();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().size());
+    }
 
 
 
