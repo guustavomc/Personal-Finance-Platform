53a541a15b10b34c32e3cab41011b33e47e93f60
guustavomc <gustavomccb@gmail.com>
2025-10-30 21:14:52 -0300

    improved summary response structure

--- investment-service/src/main/java/com/example/investment/dto/PortfolioSummaryResponse.java
+++ investment-service/src/main/java/com/example/investment/dto/PortfolioSummaryResponse.java
@@ -10,7 +10,8 @@ public class PortfolioSummaryResponse {
     private BigDecimal totalAmountInvested;
 
     private List<AssetHolding> assetList;
-    
+
+    private int totalAssets;
 
     public BigDecimal getTotalAmountInvested() {
         return totalAmountInvested;
@@ -20,11 +21,19 @@ public class PortfolioSummaryResponse {
         return assetList;
     }
 
+    public int getTotalAssets() {
+        return totalAssets;
+    }
+
     public void setTotalAmountInvested(BigDecimal totalAmountInvested) {
         this.totalAmountInvested = totalAmountInvested;
     }
 
     public void setAssetList(List<AssetHolding> assetList) {
         this.assetList = assetList;
+    }
+
+    public void setTotalAssets(int totalAssets) {
+        this.totalAssets = totalAssets;
     }
 }
