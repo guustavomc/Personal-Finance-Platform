803f9f22ad1269a16157141a70ae5bcbafad2276
guustavomc <gustavomccb@gmail.com>
2025-10-30 21:14:42 -0300

    implemented getPortfolioSummary and auxiliary methods

--- investment-service/src/main/java/com/example/investment/service/PortfolioSummaryService.java
+++ investment-service/src/main/java/com/example/investment/service/PortfolioSummaryService.java
@@ -1,6 +1,11 @@
 package com.example.investment.service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import com.example.investment.dto.PortfolioSummaryResponse;
 import com.example.investment.model.AssetHolding;
@@ -19,13 +24,35 @@ public class PortfolioSummaryService {
     public PortfolioSummaryResponse getPortfolioSummary(){
         PortfolioSummaryResponse response = new PortfolioSummaryResponse();
 
-        List<Investment> investments = investmentRepository.findAll();
+        Map<String, AssetHolding> currentHoldingMap = getStringAssetHoldingMap(
+                investmentRepository.findAll());
 
+        List<AssetHolding> assetList = new ArrayList<>(currentHoldingMap.values());
+        BigDecimal totalAmount = getAssetListTotalAmountInvested(assetList);
+
+        response.setAssetList(assetList);
+        response.setTotalAssets(assetList.size());
+        response.setTotalAmountInvested(totalAmount);
         return response;
     }
-    
 
-    public AssetHolding mapInvestmentToHolding(Investment investment){
+    private static BigDecimal getAssetListTotalAmountInvested(List<AssetHolding> assetList) {
+        return assetList.stream()
+                .map(AssetHolding::getTotalAmountInvested)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private Map<String, AssetHolding> getStringAssetHoldingMap(List<Investment> investments) {
+        Map<String, AssetHolding> currentHoldingMap = investments.stream()
+                .collect(Collectors.groupingBy(
+                        investment -> investment.getAssetSymbol() + '|' +investment.getInvestmentType(),
+                        Collectors.reducing(
+                                new AssetHolding(), this::mapInvestmentToHolding,this::mergeHoldings)));
+        return currentHoldingMap;
+    }
+
+
+    private AssetHolding mapInvestmentToHolding(Investment investment){
         AssetHolding holding = new AssetHolding();
         holding.setInvestmentType(investment.getInvestmentType());
         holding.setAssetSymbol(investment.getAssetSymbol());
@@ -36,5 +63,11 @@ public class PortfolioSummaryService {
         holding.setAlternateCurrency(investment.getAlternateCurrency());
         
         return holding;
+    }
+
+    private AssetHolding mergeHoldings(AssetHolding firstAsset, AssetHolding secondAsset) {
+        firstAsset.setTotalQuantity(firstAsset.getTotalQuantity().add(secondAsset.getTotalQuantity()));
+        firstAsset.setTotalAmountInvested(firstAsset.getTotalAmountInvested().add(secondAsset.getTotalAmountInvested()));
+        return firstAsset;
     }
 }
