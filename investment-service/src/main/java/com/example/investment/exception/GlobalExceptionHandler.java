e17199678f3c9ae4f87f35f6c54ab2a1c9127d20
guustavomc <gustavomccb@gmail.com>
2025-11-07 08:55:28 -0300

    implemented handleInvestmentNotFoundException

--- investment-service/src/main/java/com/example/investment/exception/GlobalExceptionHandler.java
+++ investment-service/src/main/java/com/example/investment/exception/GlobalExceptionHandler.java
@@ -1,8 +1,30 @@
 package com.example.investment.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvestmentNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleInvestmentNotFoundException(InvestmentNotFoundException ex){
+        return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<Map<String,Object>> buildErrorResponse(String message, HttpStatus status){
+        Map<String,Object> response = new HashMap<>();
+        response.put("timestamp", LocalDate.now());
+        response.put("status", status.value());
+        response.put("error", status.getReasonPhrase());
+        response.put("message", message);
+
+        return new ResponseEntity<>(response, status);
+    }
 }
