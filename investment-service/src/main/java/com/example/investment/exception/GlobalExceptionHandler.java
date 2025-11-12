563b89ef2b2692e88b81e06f49711bbb0c4f694f
guustavomc <gustavomccb@gmail.com>
2025-11-11 21:33:44 -0300

    updated global exception handler and added unit tests validating the exceptions

--- investment-service/src/main/java/com/example/investment/exception/GlobalExceptionHandler.java
+++ investment-service/src/main/java/com/example/investment/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.investment.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +17,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvestmentNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleInvestmentNotFoundException(InvestmentNotFoundException ex){
         return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex){
+        return buildErrorResponse("Failed to delete Investment, Database connection failed: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleDataIntegrityViolationException(DataIntegrityViolationException ex){
+        return buildErrorResponse("Failed to delete investment, the register is in use: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<Map<String,Object>> buildErrorResponse(String message, HttpStatus status){
