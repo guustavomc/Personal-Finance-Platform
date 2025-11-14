030f4eb30f804171bcd713b5b05838c67a16a7cc
guustavomc <gustavomccb@gmail.com>
2025-11-13 23:03:55 -0300

    updated portfolio summary

--- investment-service/src/main/java/com/example/investment/controller/PortfolioSummaryController.java
+++ investment-service/src/main/java/com/example/investment/controller/PortfolioSummaryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/portfolio")
+@RequestMapping("api/investment/portfolio")
 @Validated
 public class PortfolioSummaryController {
 
