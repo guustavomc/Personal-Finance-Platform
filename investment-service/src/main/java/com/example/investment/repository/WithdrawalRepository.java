dcd0f3c78ac689a34d054567a5eb8b6337ad5e86
guustavomc <gustavomccb@gmail.com>
2025-11-18 20:17:35 -0300

    Created withdrawal repository

---
+++ investment-service/src/main/java/com/example/investment/repository/WithdrawalRepository.java
@@ -0,0 +1,10 @@
+package com.example.investment.repository;
+
+import com.example.investment.model.Investment;
+import com.example.investment.model.Withdrawal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
+}
