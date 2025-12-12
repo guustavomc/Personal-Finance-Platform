5ebdd84d4aabc46805de48638607dc8db6c078c3
guustavomc <gustavomccb@gmail.com>
2025-12-11 21:31:27 -0300

    updated portfolio event class

--- investment-service/src/main/java/com/example/investment/dto/PortfolioEvent.java
+++ investment-service/src/main/java/com/example/investment/dto/PortfolioEvent.java
@@ -19,7 +19,24 @@ public class PortfolioEvent {
 
     private String currency;
 
+    private BigDecimal fee;
+
     private String eventType;
+
+    private String assetTag;
+
+
+    public PortfolioEvent(InvestmentType investmentType,String assetSymbol,BigDecimal amount, BigDecimal quantity,LocalDate date, String currency, BigDecimal fee,String eventType, String assetTag){
+        this.investmentType=investmentType;
+        this.assetSymbol=assetSymbol;
+        this.amount = amount;
+        this.quantity = quantity;
+        this.date = date;
+        this.currency = currency;
+        this.fee=fee;
+        this.eventType=eventType;
+        this.assetTag=assetTag;
+    }
 
     public InvestmentType getInvestmentType() {
         return investmentType;
@@ -45,8 +62,16 @@ public class PortfolioEvent {
         return currency;
     }
 
+    public BigDecimal getFee() {
+        return fee;
+    }
+
     public String getEventType() {
         return eventType;
+    }
+
+    public String getAssetTag() {
+        return assetTag;
     }
 
     public void setInvestmentType(InvestmentType investmentType) {
@@ -73,7 +98,15 @@ public class PortfolioEvent {
         this.currency = currency;
     }
 
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
     public void setEventType(String eventType) {
         this.eventType = eventType;
+    }
+
+    public void setAssetTag(String assetTag) {
+        this.assetTag = assetTag;
     }
 }
