15af207c3f833101c1f53e8ab15f87baa69a7a5e
guustavomc <gustavomccb@gmail.com>
2025-12-19 08:39:32 -0300

    Created new repository query

--- investment-service/src/main/java/com/example/investment/repository/WithdrawalRepository.java
+++ investment-service/src/main/java/com/example/investment/repository/WithdrawalRepository.java
@@ -5,6 +5,11 @@ import com.example.investment.model.Withdrawal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
+
+    List<Withdrawal> findByInvestmentType(String investmentType);
+
 }
