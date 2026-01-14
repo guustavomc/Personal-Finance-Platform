c61b14c3b236eaa8ba50e38252e6f5ec98737d13
guustavomc <gustavomccb@gmail.com>
2026-01-14 19:29:07 -0300

    added new sumValueSpentByDateBetween query

--- expense-service/src/main/java/com/example/expense/repository/ExpenseRepository.java
+++ expense-service/src/main/java/com/example/expense/repository/ExpenseRepository.java
@@ -1,9 +1,12 @@
 package com.example.expense.repository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.expense.model.Expense;
@@ -15,4 +18,7 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     List<Expense> findByCategory(String category);
 
     List<Expense> findByPurchaseId(String purchaseId);
+
+    @Query("SELECT SUM(e.valueSpent) FROM Expense e WHERE e.date BETWEEN :start AND :end")
+    BigDecimal sumValueSpentByDateBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
