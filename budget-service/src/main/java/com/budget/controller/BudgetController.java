fa19333bfcc38d6c9f30636b5e37464b2ba0824f
guustavomc <gustavomccb@gmail.com>
2026-01-29 22:41:59 -0300

    added createBudget on controller class

--- budget-service/src/main/java/com/budget/controller/BudgetController.java
+++ budget-service/src/main/java/com/budget/controller/BudgetController.java
@@ -1,4 +1,38 @@
 package com.budget.controller;
 
+import com.budget.dto.BudgetResponse;
+import com.budget.dto.CreateBudgetRequest;
+import com.budget.service.BudgetService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/budgets")
 public class BudgetController {
+
+    @Autowired
+    private BudgetService budgetService;
+
+    public BudgetController(BudgetService budgetService){
+        this.budgetService=budgetService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BudgetResponse> createBudget(@Valid @RequestBody CreateBudgetRequest createBudgetRequest){
+        BudgetResponse response = new BudgetResponse();
+        try {
+            response = budgetService.saveBudget(createBudgetRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+    }
+
 }
