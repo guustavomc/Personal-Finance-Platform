c02b8b9820dd57838abff04ad135f647c7fb7c6b
guustavomcc <gustavomccb@gmail.com>
2026-02-02 17:39:44 -0300

    added InvestmentResponse class and implemented getInvestmentsByMonth method in InvestmentServiceClient

---
+++ budget-service/src/main/java/com/budget/dto/InvestmentResponse.java
@@ -0,0 +1,105 @@
+package main.java.com.budget.dto;
+
+public class InvestmentResponse {
+
+    private long id;
+
+    private InvestmentType investmentType;
+
+    private String assetSymbol;
+
+    private BigDecimal amountInvested;
+
+    private  BigDecimal quantity;
+
+    private LocalDate investmentDate;
+
+    private String currency;
+
+    private BigDecimal alternateAmount;
+
+    private String alternateCurrency;
+
+    private String assetTag;
+
+    public long getId() {
+        return id;
+    }
+
+    public InvestmentType getInvestmentType() {
+        return investmentType;
+    }
+
+    public String getAssetSymbol() {
+        return assetSymbol;
+    }
+
+    public BigDecimal getAmountInvested() {
+        return amountInvested;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public LocalDate getInvestmentDate() {
+        return investmentDate;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getAlternateAmount() {
+        return alternateAmount;
+    }
+
+    public String getAlternateCurrency() {
+        return alternateCurrency;
+    }
+
+    public String getAssetTag() {
+        return assetTag;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setInvestmentType(InvestmentType investmentType) {
+        this.investmentType = investmentType;
+    }
+
+    public void setAssetSymbol(String assetSymbol) {
+        this.assetSymbol = assetSymbol;
+    }
+
+    public void setAmountInvested(BigDecimal amountInvested) {
+        this.amountInvested = amountInvested;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setInvestmentDate(LocalDate investmentDate) {
+        this.investmentDate = investmentDate;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency=currency;
+
+    }
+
+    public void setAlternateAmount(BigDecimal alternateAmount) {
+        this.alternateAmount = alternateAmount;
+    }
+
+    public void setAlternateCurrency(String alternateCurrency) {
+        this.alternateCurrency = alternateCurrency;
+    }
+
+    public void setAssetTag(String assetTag) {
+        this.assetTag = assetTag;
+    }
+}
--- budget-service/src/main/java/com/budget/service/InvestmentServiceClient.java
+++ budget-service/src/main/java/com/budget/service/InvestmentServiceClient.java
@@ -1,4 +1,30 @@
 package com.budget.service;
 
+import java.util.List;
+
+import com.budget.dto.ExpenseResponse;
+
+import main.java.com.budget.dto.InvestmentResponse;
+
+@Component
 public class InvestmentServiceClient {
+
+    @Value("${investment.service.url}")
+    private String investmentServiceURL;
+
+    private final RestClient restClient;
+
+    public InvestmentServiceClient(RestClient.Builder builder){
+        this.restClient = builder.baseUrl(investmentServiceURL).build();
+    }
+
+    public List<ExpenseResponse> getInvestmentsByMonth(int year, int month){
+        String uri = "/api/investment/report/monthly?year={year}&month={month}";
+
+        return restClient.get()
+                .uri(uri, year, month)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<InvestmentResponse>>() {
+        });
+    }
 }
