e437b77eec44a33602ceac9f2654b3a6d484df9e
guustavomc <gustavomccb@gmail.com>
2026-02-16 20:51:52 -0300

    added Delete and Put mapping on budget controller

--- budget-service/src/main/java/com/budget/controller/BudgetController.java
+++ budget-service/src/main/java/com/budget/controller/BudgetController.java
@@ -20,39 +20,49 @@ public class BudgetController {
     private BudgetService budgetService;
 
     @GetMapping({"/{id}"})
-    public ResponseEntity<BudgetResponse> getBudgetById(@PathVariable("id") long id){
+    public ResponseEntity<BudgetResponse> getBudgetById(@PathVariable("id") long id) {
         BudgetResponse response = new BudgetResponse();
         try {
             response = budgetService.findBudgetWithID(id);
             return ResponseEntity.status(HttpStatus.OK).body(response);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
     }
 
     @GetMapping({"/{id}/refresh"})
-    public ResponseEntity<BudgetResponse> getRefreshedBudgetById(@PathVariable("id") long id){
+    public ResponseEntity<BudgetResponse> getRefreshedBudgetById(@PathVariable("id") long id) {
         BudgetResponse response = new BudgetResponse();
         try {
             response = budgetService.findUpdatedBudgetWithID(id);
             return ResponseEntity.status(HttpStatus.OK).body(response);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
     }
 
     @PostMapping
-    public ResponseEntity<BudgetResponse> createBudget(@Valid @RequestBody CreateBudgetRequest createBudgetRequest){
+    public ResponseEntity<BudgetResponse> createBudget(@Valid @RequestBody CreateBudgetRequest createBudgetRequest) {
         BudgetResponse response = new BudgetResponse();
         try {
             response = budgetService.saveBudget(createBudgetRequest);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
     }
 
+    @DeleteMapping({"/{id}"})
+    public ResponseEntity<BudgetResponse> deleteBudget(@PathVariable("id") long id) {
+        budgetService.removeBudgetWithID(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping({"/{id}"})
+    public ResponseEntity<BudgetResponse> updateBudget(@PathVariable("id") long id, @Valid @RequestBody CreateBudgetRequest createBudgetRequest) {
+        BudgetResponse response = new BudgetResponse();
+        response = budgetService.editBudgetByID(id, createBudgetRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
 }
