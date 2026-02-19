dc2c4d34ed30749920ca14de28c540f624fcecd2
guustavomcc <gustavomccb@gmail.com>
2026-02-19 17:32:47 -0300

    add GlobalExceptionHandlerTest for BudgetController

---
+++ budget-service/src/test/java/com/budget/controller/GlobalExceptionHandlerTest.java
@@ -0,0 +1,18 @@
+package com.budget.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.budget.service.BudgetService;
+
+@WebMvcTest(BudgetController.class)
+public class GlobalExceptionHandlerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private BudgetService budgetService;
+    
+}
