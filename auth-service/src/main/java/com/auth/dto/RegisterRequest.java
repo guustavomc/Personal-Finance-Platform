7825b7d68ba8a1c7de855f047b95b5955bf157df
guustavomc <gustavomccb@gmail.com>
2026-03-21 13:27:16 -0300

    Add DTO classes for authentication requests and responses

---
+++ auth-service/src/main/java/com/auth/dto/RegisterRequest.java
@@ -0,0 +1,49 @@
+package com.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+public class RegisterRequest {
+
+
+    @NotBlank(message = "Username is required")
+    @Size(min=3, max=50)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
